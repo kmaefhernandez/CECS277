392149a12da717c2ab03339c13b990527549e931
Jon Ham <jondwoo@gmail.com>
2017-10-20 19:46:34 -0700

    changed from to 3 scoop ice cream
    requirements: max 3 scoops. 
    changed from 4 scoop chocolate to 3 scoops.

--- Mimi-s Ice Cream Shop/IceCreamConeTester.java
+++ Mimi-s Ice Cream Shop/IceCreamConeTester.java
@@ -41,7 +41,7 @@ public class IceCreamConeTester {
         caretaker.addMemento(ICC.save());
 
         System.out.println("CREATING NEW CONE...");
-        ICC = new AdvancedIceCreamCone(3, "strawberry", "waffle");
+        ICC = new AdvancedIceCreamCone(2, "strawberry", "waffle");
         displayNewCone(ICC);
 
         System.out.println("Adding scoop..");
@@ -71,7 +71,7 @@ public class IceCreamConeTester {
         caretaker.addMemento(ICC.save());
 
         System.out.println("CREATING NEW CONE...");
-        ICC = new AdvancedIceCreamCone(2, "vanilla", "regular");
+        ICC = new AdvancedIceCreamCone(0, "vanilla", "regular");
         displayNewCone(ICC);
 
         System.out.println("Adding toppings: fruity pebbles, blueberries, banana");
