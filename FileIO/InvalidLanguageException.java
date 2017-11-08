8eb44b4ac25059114819ee0034ba134e192abbd4
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2017-11-08 14:52:54 -0800

    Update InvalidLanguageException.java

--- FileIO/InvalidLanguageException.java
+++ FileIO/InvalidLanguageException.java
@@ -1,9 +1,28 @@
+/* Jon Ham
+ * Kristen Mae Hernandez
+ * November 8, 2017
+ * Purpose: The purpose of this program is to handle invalid 
+ * language exceptions.
+ */
 package Projects.FileIO;
 
+/**
+ * This program extends the IllegalArgumentException and is used to
+ * handle invalid language exceptions.
+ * @author Kristen Mae Hernandez
+ * @author Jon Ham
+ */
 public class InvalidLanguageException extends IllegalArgumentException {
-    public InvalidLanguageException(){
+    
+	/** Default Constructor */
+	public InvalidLanguageException(){
 
     }
+	
+	/**
+	 * Overloaded Constructor
+	 * @param message the message to inform the user of the exception error
+	 */
     public InvalidLanguageException(String message){
         super(message);
     }
