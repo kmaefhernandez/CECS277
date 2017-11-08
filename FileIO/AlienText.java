43e0a64006394da776353af590f6bc739bd534fc
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2017-11-08 13:52:00 -0800

    Update AlienText.java

--- FileIO/AlienText.java
+++ FileIO/AlienText.java
@@ -1,22 +1,41 @@
+/* Jon Ham
+ * Kristen Mae Hernandez
+ * November 9, 2017
+ * Purpose: The purpose of this class is to create an AlienText.
+ * An AlienText is not in standard English, it's in a foreign language.
+ */
 package Projects.FileIO;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 
+ * @author Jon Ham
+ * @author Kristen Mae Hernandez
+ */
 public class AlienText implements AlienCellPhone {
     private String languageType;
 
+    /** Default Constructor
+     * @param languageType The type of language
+     */
     public AlienText(String languageType){
         this.languageType = languageType;
     }
 
-    @Override
+    /**
+     * Sends a text
+     */
     public void sendText(String languageType, String fileName){
         //no need to send
     }
 
-    @Override
+    /**
+     * Reads an alien text from file
+     * @param languageType the type of the language
+     * @param fileName The name of the file being used
+     */
     public void readText(String languageType, String fileName){
-        //read alien text from file
         try{
             Scanner in = new Scanner(new File(fileName));
             while(in.hasNextLine()){
@@ -28,10 +47,15 @@ public class AlienText implements AlienCellPhone {
         }
     }
 
-    @Override
+    /**
+     * Translates English to Alien text
+     * @param languageType The type of language
+     * @param message The text message
+     * @param fileName The name of the file being used
+     */
     public void translateText(String languageType, char[] message, String fileName){
-        //translate english to alien text
-        try{
+
+    	try{
             if(languageType.equalsIgnoreCase("klingon")){
                 System.out.println("Translating to Klingon...");
                 //algorithm to translate english to klingon
