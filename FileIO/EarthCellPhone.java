65792694e85c087e4486a5db81680105002d48c1
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2017-11-08 14:39:18 -0800

    Update EarthCellPhone.java

--- FileIO/EarthCellPhone.java
+++ FileIO/EarthCellPhone.java
@@ -1,6 +1,30 @@
+/* Jon Hame
+ * Kristen Mae Hernandez
+ * November 9, 2017
+ * Purpose: The purpose of this program is to declare the methods that
+ * will be used in the EarthText class 
+ */
 package Projects.FileIO;
 
+/**
+ * Uses the English Language. It is able to send and read messages.
+ * @author Jon Ham
+ * @author Kristen Mae Hernandez
+ */
 public interface EarthCellPhone {
-    public void sendMessage(String languageType, char[] message, String fileName) throws InvalidLanguageException;
-    public void readMessage(String languageType, String fileName);
+    /**
+     * Sends a text messages
+     * @param languageType The type of language
+     * @param message The text message
+     * @param fileName The name of the file
+     * @throws InvalidLanguageException An Invalid Language Exception
+     */
+	public void sendMessage(String languageType, char[] message, String fileName) throws InvalidLanguageException;
+    
+	/**
+	 * Reads text messages
+	 * @param languageType The type of language
+	 * @param fileName The name of the file
+	 */
+	public void readMessage(String languageType, String fileName);
 }
